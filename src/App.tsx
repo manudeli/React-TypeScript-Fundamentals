3f68ed4fce4add6bb7452b17169405a0697a6897
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-04 18:17:39 +0900

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
-// 요구사항
-// 1. Counter 컴포넌트 구현하기
-// 2. 모든 Counter 컴포넌트의 합 구하기
-
-// 무엇을 배울 것인가
-// 1. 컴포넌트에서 지역 상태 관리하는 법
-// 2. 컴포넌트에 이벤트 바인딩하기
-// 3. 부모 컴포넌트에게 메시지 전달하기
-
-import { useState } from 'react'
-import Counter from './components/Counter'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [totalCount, setTotalCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-  const handleTotalCountIncrease = () => setTotalCount(totalCount + 1)
-  const handleTotalCountDecrease = () => setTotalCount(totalCount - 1)
+  useEffect(() => {
+    console.log(`Clicked ${count} times.`)
+  }, [count]) // count의 변화를 감지한다.
+
+  useEffect(() => {
+    console.log('Component Loaded')
+    const handleScroll = () => {
+      console.log(window.scrollY)
+    }
+
+    document.addEventListener('scroll', handleScroll) // 전역적인 이벤트를 사용할 때 쓸 수 있다.
+    return () => document.removeEventListener('scroll', handleScroll) // return으로 반환한 함수는 컴포넌트가 제거될 때 실행된다.
+  }, []) // 컴포넌트가 처음 로드될 때 실행된다.
 
   return (
     <div className="App">
-      TotalCount: {totalCount}
-      <Counter
-        onIncrease={handleTotalCountIncrease}
-        onDecrease={handleTotalCountDecrease}
-      />
-      <Counter
-        onIncrease={handleTotalCountIncrease}
-        onDecrease={handleTotalCountDecrease}
-      />
-      <Counter
-        onIncrease={handleTotalCountIncrease}
-        onDecrease={handleTotalCountDecrease}
-      />
-      <Counter
-        onIncrease={handleTotalCountIncrease}
-        onDecrease={handleTotalCountDecrease}
-      />
+      <div>You clicked {count} times.</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+
+      <div style={{ height: 10000 }}></div>
     </div>
   )
 }

368fc3b147bb2b539645498b885b0886850e56b1
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-05 18:15:38 +0900

    Custom Hook: useHover

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { RefObject } from 'react'
+import Box from './components/Box'
 import Checkbox from './components/Checkbox'
+import useHover from './hooks/useHover'
 import useToggle from './hooks/useToggle'
 
 function App() {
   const [on, toggle] = useToggle(false)
-
-  console.log(on)
+  const [ref, isHover] = useHover()
 
   return (
     <div>
       <Checkbox label="하이" checked={on} onChange={toggle} />
       <Checkbox label="하이" checked={on} onChange={toggle} />
       <Checkbox label="하이" checked={on} onChange={toggle} />
+      {isHover ? 'hover' : 'mouseout'}
+      <Box ref={ref as RefObject<HTMLDivElement>} />
     </div>
   )
 }
---
+++ src/components/Box.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+`
+
+export default Box

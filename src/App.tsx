4c3081c92c3b51a8c36ab18526798f9e72beb51a
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-05 18:22:42 +0900

    Custom Hook: useKeyPress

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RefObject } from 'react'
 import Box from './components/Box'
 import Checkbox from './components/Checkbox'
 import useHover from './hooks/useHover'
+import useKeyPress from './hooks/useKeyPress'
 import useToggle from './hooks/useToggle'
 
 function App() {
   const [on, toggle] = useToggle(false)
   const [ref, isHover] = useHover()
+  const keyPressed = useKeyPress('a')
 
   return (
     <div>
@@ -15,6 +17,8 @@ function App() {
       <Checkbox label="하이" checked={on} onChange={toggle} />
       {isHover ? 'hover' : 'mouseout'}
       <Box ref={ref as RefObject<HTMLDivElement>} />
+
+      {keyPressed && 'Pressed'}
     </div>
   )
 }

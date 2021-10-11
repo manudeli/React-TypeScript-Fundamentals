a7f092f540c6fe2d9a394d14e64b22ef35c65046
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-11 14:53:50 +0900

    Components: Spinner

---
+++ src/stories/Spinner.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Spinner from '../components/Spinner'
+
+export default {
+  title: 'Component/Spinner',
+  component: Spinner,
+  argTypes: {
+    size: {
+      control: { type: 'range', min: 24, max: 200 },
+    },
+  },
+} as ComponentMeta<typeof Spinner>
+
+export const Default: ComponentStory<typeof Spinner> = (args) => {
+  return <Spinner {...args} />
+}

872a5c6e0d2dbb52daccabcabc853c26be135edd
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-16 23:18:03 +0900

    Hooks: useKey

---
+++ src/hooks/useKey.ts
@@ -0,0 +1,31 @@
+import { useEffect } from '@storybook/client-api'
+import { useCallback } from 'react'
+import { EventKeyValue } from './EventKeyValueType'
+
+type KeyEvent = 'keydown' | 'keyup'
+type Handler = () => void
+
+const useKey = (
+  event: KeyEvent = 'keydown',
+  targetKey: EventKeyValue,
+  handler: Handler
+) => {
+  const handleKey = useCallback(
+    ({ key }) => {
+      if (key === targetKey) {
+        handler()
+      }
+    },
+    [targetKey, handler]
+  )
+
+  useEffect(() => {
+    window.addEventListener(event, handleKey)
+
+    return () => {
+      window.removeEventListener(event, handleKey)
+    }
+  }, [event, targetKey, handleKey])
+}
+
+export default useKey
